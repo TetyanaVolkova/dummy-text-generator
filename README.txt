b2757226d0ba8f9fac2cb1d24ed8aa6cf1d2078c
Tetyana Volkova <tatankavolkova@gmail.com>
2017-09-10 02:37:13 -0400

    Dummy text generator.

---
+++ README.txt
@@ -0,0 +1,12 @@
+ To create node_modules, run
+npm install
+
+
+To start webpack
+webpack -w
+
+Install live-server
+npm install -g live-server
+
+Run live-server
+live-server OR live-server --port=8000
---
+++ src/Components/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import Output from './Output';
+import Text from './Controls/Text';
+import Select from './Controls/Select';
+import axios from 'axios';
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      html: false,
+      text:''
+    }
+  }
+  componentWillMount() {
+    this.getText();
+  }
+
+  getText() {
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+      .then((response) => {
+        this.setState({text: response.data.text}, function() {
+          //console.log(this.state);
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  changeParas(number) {
+    this.setState({paras: number}, this.getText);
+  }
+  showHtml(h) {
+    this.setState({html:h}, this.getText)
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1>Dummy Text Generator</h1>
+        <Output value={this.state.text}/>
+        <h3>Real time Options</h3>
+        <form>
+          <div>
+            <label>Paragraphs: </label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)}/>
+          </div>
+            <div>
+              <label>Include HTML: </label>
+              <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+            </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
